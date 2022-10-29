00d89d121f0a9f0a3efd69cb116a87025ed0ce78
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2022-10-29 10:50:44 +0530

    added env resolver and gin context util

--- server/graph/schema.resolvers.go
+++ server/graph/schema.resolvers.go
@@ -5,15 +5,15 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/ArkamFahry/GateGuardian/server/graph/generated"
 	"github.com/ArkamFahry/GateGuardian/server/graph/model"
+	"github.com/ArkamFahry/GateGuardian/server/resolvers"
 )
 
 // Env is the resolver for the _env field.
 func (r *queryResolver) Env(ctx context.Context) (*model.Env, error) {
-	panic(fmt.Errorf("not implemented: Env - _env"))
+	return resolvers.EnvResolver(ctx)
 }
 
 // Query returns generated.QueryResolver implementation.
---
+++ server/resolvers/env.go
@@ -0,0 +1,32 @@
+package resolvers
+
+import (
+	"context"
+
+	"github.com/ArkamFahry/GateGuardian/server/constants"
+	"github.com/ArkamFahry/GateGuardian/server/db/memorydb"
+	"github.com/ArkamFahry/GateGuardian/server/graph/model"
+)
+
+func EnvResolver(ctx context.Context) (*model.Env, error) {
+	res := &model.Env{}
+
+	dbUrl, _ := memorydb.Provider.GetEnvByKey(constants.DatabaseURL)
+	res.DatabaseURL = &dbUrl
+	dbName, _ := memorydb.Provider.GetEnvByKey(constants.DatabaseName)
+	res.DatabaseName = &dbName
+	dbNameSpace, _ := memorydb.Provider.GetEnvByKey(constants.DatabaseNameSpace)
+	res.DatabaseNamespace = &dbNameSpace
+	dbUserName, _ := memorydb.Provider.GetEnvByKey(constants.DatabaseUsername)
+	res.DatabaseUsername = &dbUserName
+	port, _ := memorydb.Provider.GetEnvByKey(constants.Port)
+	res.Port = &port
+	jwtType, _ := memorydb.Provider.GetEnvByKey(constants.JwtType)
+	res.JwtType = &jwtType
+	jwtSecret, _ := memorydb.Provider.GetEnvByKey(constants.JwtSecret)
+	res.JwtSecret = &jwtSecret
+	clientId, _ := memorydb.Provider.GetEnvByKey(constants.ClientID)
+	res.ClientID = &clientId
+
+	return res, nil
+}
