ed22d89b4529a41b9d429990c0467e3b127444de
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2022-10-30 20:40:45 +0530

    added signup input gql schema

--- server/db/maindb/models/user.go
+++ server/db/maindb/models/user.go
@@ -7,11 +7,10 @@ type User struct {
 	EmailVerifiedAt          *int64  `json:"email_verified_at"`
 	Password                 *string `json:"password"`
 	SignUpMethods            string  `json:"sign_up_method"`
-	GivenName                *string `json:"given_name"`
+	UserName                 *string `json:"user_name"`
 	FirstName                *string `json:"first_name"`
 	MiddleName               *string `json:"middle_name"`
 	LastName                 *string `json:"last_name"`
-	FullName                 *string `json:"full_name"`
 	NickName                 *string `json:"nick_name"`
 	Gender                   *string `json:"gender"`
 	BirthDate                *string `json:"birth_date"`
@@ -19,9 +18,6 @@ type User struct {
 	PhoneNumberVerifiedAt    *int64  `json:"phone_number_verified_at"`
 	Picture                  *string `json:"picture"`
 	Roles                    string  `json:"roles"`
-	DefaultRole              string  `json:"default_role"`
-	AssignedRole             string  `json:"assigned_role"`
-	AssignedRoles            string  `json:"assigned_roles"`
 	RevokedTimestamp         *int64  `json:"revoked_timestamp"`
 	IsMultiFactorAuthEnabled *bool   `json:"is_multi_factor_auth_enabled"`
 	UpdatedAt                int64   `json:"updated_at"`
