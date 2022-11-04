f4c9160fd4ab6f76bf5447a5edf50633d60c06a8
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2022-11-04 16:54:31 +0530

    changed maindb to db

---
+++ server/db/db.go
@@ -0,0 +1,42 @@
+package db
+
+import (
+	"github.com/ArkamFahry/GateGuardian/server/constants"
+	"github.com/ArkamFahry/GateGuardian/server/db/providers"
+	"github.com/ArkamFahry/GateGuardian/server/db/providers/genjidb"
+	"github.com/ArkamFahry/GateGuardian/server/env"
+	"github.com/sirupsen/logrus"
+)
+
+var Provider providers.Provider
+
+func InitMainDB() error {
+	var err error
+
+	DbType, _ := env.GetEnvByKey(constants.DatabaseType)
+
+	isGenjiDb := DbType == constants.DbTypeGenjiDb
+	// isSurrealDb := DbType == constants.DbTypeSurrealDb
+
+	if isGenjiDb {
+		Provider, err = genjidb.NewGenjiDbProvider()
+		if err != nil {
+			logrus.Fatal("Failed Initializing surrealdb: ", err)
+			return err
+		} else {
+			logrus.Info("Surrealdb successfully initialized")
+		}
+	}
+
+	// if isSurrealDb {
+	// 	Provider, err = surrealdb.NewSurrealDbProvider()
+	// 	if err != nil {
+	// 		logrus.Fatal("Failed Initializing surrealdb: ", err)
+	// 		return err
+	// 	} else {
+	// 		logrus.Info("Surrealdb successfully initialized")
+	// 	}
+	// }
+
+	return nil
+}
