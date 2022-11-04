0c829b2ea952991737e13208988057d47ab047b6
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2022-11-04 16:53:58 +0530

    added db type genjidb

--- server/constants/db_types.go
+++ server/constants/db_types.go
@@ -3,4 +3,6 @@ package constants
 const (
 	// DbTypeSurrealDb is the surrealdb database type
 	DbTypeSurrealDb = "surrealdb"
+	// DbTypeGenjiDb is the genjidb database type
+	DbTypeGenjiDb = "genjidb"
 )
