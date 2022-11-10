6741b9de98e43d9c930900fadaee3b317efb1191
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2022-11-10 18:07:54 +0530

    removed models

--- server/db/models/user.go
+++ server/db/models/user.go
@@ -1,6 +1,5 @@
 package models
 
-// DB model
 type User struct {
 	Id         string `json:"id" db:"id"`
 	Email      string `json:"email" db:"email"`
@@ -14,37 +13,4 @@ type User struct {
 	Picture    string `json:"picture" db:"picture"`
 	CreatedAt  int64  `json:"created_at" db:"created_at"`
 	UpdatedAt  int64  `json:"updated_at" db:"updated_at"`
-}
-
-// APi Models
-type UserSignUp struct {
-	Email            string `json:"email"`
-	Password         string `json:"password"`
-	Confirm_Password string `json:"confirm_password"`
-	GivenName        string `json:"given_name"`
-	FamilyName       string `json:"family_name"`
-	MiddleName       string `json:"middle_name"`
-	NickName         string `json:"nick_name"`
-	Gender           string `json:"gender"`
-	BirthDate        string `json:"birth_date"`
-	Picture          string `json:"picture"`
-}
-
-type UserSignIn struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type ApiUser struct {
-	Id         string `json:"id"`
-	Email      string `json:"email"`
-	GivenName  string `json:"given_name"`
-	FamilyName string `json:"family_name"`
-	MiddleName string `json:"middle_name"`
-	NickName   string `json:"nick_name"`
-	Gender     string `json:"gender"`
-	BirthDate  string `json:"birth_date"`
-	Picture    string `json:"picture"`
-	CreatedAt  int64  `json:"created_at"`
-	UpdatedAt  int64  `json:"updated_at"`
 }
