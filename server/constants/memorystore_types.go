90b80fb3402edb9c6a96081c67badfbaede88d58
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-01-05 11:03:41 +0530

    added constants for object and memorystore type

---
+++ server/constants/memorystore_types.go
@@ -0,0 +1,18 @@
+package constants
+
+const (
+	// MemoryStoreTypeRedis is the redis memorystore type
+	MemoryStoreTypeRedis = "redis"
+
+	// MemoryStoreTypeDragonFly is the dragonfly memorystore type
+	MemoryStoreTypeDragonFly = "dragonfly"
+
+	// MemoryStoreTypeKeyDb is the keydb memorystore type
+	MemoryStoreTypeKeyDb = "keydb"
+
+	// MemoryStoreTypeKvrocks is the kvrocks memorystore type
+	MemoryStoreTypeKvrocks = "kvrocks"
+
+	// MemoryStoreTypeInmemory is the inmemory memorystore type
+	MemoryStoreTypeInmemory = "inmemory"
+)
---
+++ server/constants/objectstore_types.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	// ObjectStoreTypeMinio is the minio objectstore type
+	ObjectStoreTypeMinio = "minio"
+
+	// ObjectStoreTypeRook is the rook objectstore type
+	ObjectStoreTypeRook = "rook"
+
+	// ObjectStoreTypeLongHorn is the longhorn objectstore type
+	ObjectStoreTypeLongHorn = "longhorn"
+)
