e4c17d66d792354927b98e63f1e6aff3d87c95e1
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-01-05 22:11:32 +0530

    setup server to retrieve port from env

--- server/cmd/serve.go
+++ server/cmd/serve.go
@@ -1,9 +1,11 @@
 package cmd
 
 import (
+	"gategaurdian/server/database/memorystore"
 	"gategaurdian/server/handlers"
 
 	"github.com/gofiber/fiber/v2"
+	log "github.com/sirupsen/logrus"
 )
 
 // Serve function servers the newly bootstrapped server
@@ -15,7 +17,12 @@ func Serve() error {
 	app.All("/graphql", handlers.Graphql)
 	app.All("/", handlers.PlayGround)
 
-	err = app.Listen(":3000")
+	port := memorystore.RequiredEnvStoreObj.GetRequiredEnv().Port
+
+	err = app.Listen(":" + port)
+	if err != nil {
+		log.Error("Error starting the server : ", err)
+	}
 
 	return err
 }
